5a26428db7f05326e68b3010fdd154c8e1c7c283
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-01-19 16:11:43 +0000

    add textarea in from-text page

--- frontend/src/routes/from-text/+page.svelte
+++ frontend/src/routes/from-text/+page.svelte
@@ -5,6 +5,10 @@
 <main class="flex flex-col w-96 h-svh p-5">
     <section class="w-full grow">
         <h1 class="w-full mb-5 text-4xl">テキストから</h1>
+        <textarea
+            class="w-full h-80 resize-none"
+            placeholder="予定の情報を入力"
+        />
     </section>
     <section class="w-full">
         <SubmitButton>送信</SubmitButton>
