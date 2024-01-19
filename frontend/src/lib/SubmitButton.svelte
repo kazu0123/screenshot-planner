58a05084d283441aed7cfbdcdec7e1a142a31cce
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-01-19 14:28:17 +0000

    fix SubmitButton style

--- frontend/src/lib/SubmitButton.svelte
+++ frontend/src/lib/SubmitButton.svelte
@@ -1,10 +1,37 @@
-<button type="button" class="
-    w-full h-16 rounded-lg
-    text-white bg-blue-700 hover:bg-blue-800
-    focus:ring-4 focus:ring-blue-300 focus:outline-none
-    dark:bg-blue-600 dark:hover:bg-blue-700
-    font-medium text-2xl
-    dark:focus:ring-blue-800"
->
+<button type="button">
     <slot />
 </button>
+
+<style>
+    button {
+        --sky-50: rgb(240 249 255);
+        --sky-300: rgb(125 211 252);
+        --sky-600: rgb(2 132 199);
+
+        width: 100%;
+        height: 4rem;
+        outline: none;
+
+        background-color: var(--sky-600);
+
+        border-radius: 0.5rem;
+        border-width: 2px;
+        border-color: var(--sky-50);
+
+        color: var(--sky-50);
+        font-size: 1.5rem;
+        line-height: 2rem;
+        font-weight: 500;
+    }
+
+    button:active {
+        background-color: var(--sky-50);
+
+        border-color: var(--sky-600);
+        color: var(--sky-600);
+    }
+
+    button:focus {
+        box-shadow: 0 0 0 4px var(--sky-300);
+    }
+</style>
