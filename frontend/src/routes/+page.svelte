16499a093c79f9ea563f957449b9279954657d7d
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-01-19 08:14:21 +0000

    fix style

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,7 +2,7 @@
     import BottomButton from "$lib/BottomButton.svelte";
 </script>
 
-<main class="flex justify-center items-center w-96 h-svh">
+<main class="flex justify-center items-center w-96 h-full">
     <h1 class="relative bottom-24 text-4xl">予定登録アプリ</h1>
     <ul class="flex absolute bottom-5">
         <li class="first:ml-0 ml-2"><a href="/from-text"><BottomButton icon="text"></BottomButton></a></li>
