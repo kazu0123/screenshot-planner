33cf6a82fd3b443e4b4e9ebba10fb61ce53535e2
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-01-19 16:11:05 +0000

    add SubmitButton in from-text page

--- frontend/src/routes/from-text/+page.svelte
+++ frontend/src/routes/from-text/+page.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
+    import SubmitButton from "$lib/SubmitButton.svelte";
 </script>
 
-<main class="w-96 h-svh p-5">
-    <h1 class="mb-1 text-4xl">テキストから</h1>
+<main class="flex flex-col w-96 h-svh p-5">
+    <section class="w-full grow">
+        <h1 class="w-full mb-5 text-4xl">テキストから</h1>
+    </section>
+    <section class="w-full">
+        <SubmitButton>送信</SubmitButton>
+    </section>
 </main>
