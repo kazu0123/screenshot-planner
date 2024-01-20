19b8d1c690b937ffc24808b3aede3d4eec07f5ba
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-01-20 10:46:12 +0000

    add cors

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from PIL import UnidentifiedImageError
 from fastapi import FastAPI, UploadFile, HTTPException
+from starlette.middleware.cors import CORSMiddleware
 
 from load_image import load_image
 from image_to_string import image_to_string
@@ -11,6 +12,20 @@ from parse_event_details import router as parse_event_details_router
 app = FastAPI()
 app.include_router(create_event_router)
 app.include_router(parse_event_details_router)
+
+origins = [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "http://app.localhost",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post('/image-to-text')
 async def image_to_text(file: UploadFile):
