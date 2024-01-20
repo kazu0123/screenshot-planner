45b61611e6769a5c550462686bdd83ec34837a41
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-01-20 17:59:55 +0000

    add type guard

--- frontend/src/lib/CalendarEvent.ts
+++ frontend/src/lib/CalendarEvent.ts
@@ -1,3 +1,5 @@
+import { isNotNullish } from "./isNotNullish";
+
 export type CalendarEvent = {
     event_title: string,
     event_description: string,
@@ -6,3 +8,16 @@ export type CalendarEvent = {
     end_datetime: string,
     recurrence: string,
 };
+
+export const isCalendarEvent = (item: unknown): item is CalendarEvent => {
+    if (!isNotNullish(item)) return false;
+
+    return (
+        typeof (item.event_title) === "string" &&
+        typeof (item.event_description) === "string" &&
+        typeof (item.event_location) === "string" &&
+        typeof (item.start_datetime) === "string" &&
+        typeof (item.end_datetime) === "string" &&
+        typeof (item.recurrence) === "string"
+    )
+}
