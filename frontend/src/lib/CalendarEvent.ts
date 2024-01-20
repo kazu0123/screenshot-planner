8091253ca7153578e5e40ecadc6c3e0b3641d168
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-01-20 17:06:33 +0000

    add CalendarEvent type

---
+++ frontend/src/lib/CalendarEvent.ts
@@ -0,0 +1,8 @@
+export type CalendarEvent = {
+    event_title: string,
+    event_description: string,
+    event_location: string,
+    start_datetime: string,
+    end_datetime: string,
+    recurrence: string,
+};
