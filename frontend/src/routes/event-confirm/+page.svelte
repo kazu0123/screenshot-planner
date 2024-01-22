d8de6b8973e88d5867eb8db2aa57587e940ad8f4
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-01-22 02:54:34 +0000

    add event-confirm page

---
+++ frontend/src/routes/event-confirm/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { writable, type Writable } from "svelte/store";
+    import { type CalendarEvent } from "$lib/CalendarEvent";
+
+    let calendarEvents: Writable<CalendarEvent[]> = writable([]);
+</script>
+
+<ul>
+{#each $calendarEvents as calendarEvent}
+    <li>
+        <h2>{calendarEvent.event_title}</h2>
+        <p>{calendarEvent.start_datetime}</p>
+        <p>{calendarEvent.end_datetime}</p>
+    </li>
+{/each}
+</ul>
