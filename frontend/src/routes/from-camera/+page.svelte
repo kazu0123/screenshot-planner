50764a680480a52f13774a70c4f1c54cb22fb0ca
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-01-23 16:19:36 +0000

    add: from-camera ページを追加

---
+++ frontend/src/routes/from-camera/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import SubmitButton from "$lib/SubmitButton.svelte";
+    import { Spinner } from "flowbite-svelte";
+    import { writable } from "svelte/store";
+
+    const processing = writable(false);
+</script>
+
+<main class="flex flex-col w-96 h-svh p-5">
+    <section class="w-full grow">
+        <h1 class="w-full mb-5 text-4xl">画像から</h1>
+    </section>
+    <section class="w-full">
+        <SubmitButton disabled={$processing}>
+            {#if $processing}
+                <Spinner></Spinner>
+            {:else}
+                送信
+            {/if}
+        </SubmitButton>
+    </section>
+</main>
