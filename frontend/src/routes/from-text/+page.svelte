6e4629c0c10c8b4ba6540a79b5ebfa117e8334ff
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-01-23 14:08:35 +0000

    add: API送信&セッションストレージに保存

--- frontend/src/routes/from-text/+page.svelte
+++ frontend/src/routes/from-text/+page.svelte
@@ -1,16 +1,38 @@
 <script lang="ts">
+    import { isCalendarEvent } from "$lib/CalendarEvent";
     import SubmitButton from "$lib/SubmitButton.svelte";
+    import { writable } from "svelte/store";
+
+    const textareaValue = writable("");
+
+    async function handleClick(event: MouseEvent) {
+        const requestURL = new URL("http://127.0.0.1:8000/parse-event-details");
+        requestURL.searchParams.append("message", $textareaValue);
+
+        const response = await fetch(requestURL, {
+            method: "POST",
+        });
+
+        const data = await response.json();
+
+        if (!Array.isArray(data) || !data.every(isCalendarEvent)) {
+            throw new Error("Unexpected response format from /parse-event-details");
+        }
+
+        sessionStorage.setItem("calendarEvents", JSON.stringify(data));
+    }
 </script>
 
 <main class="flex flex-col w-96 h-svh p-5">
     <section class="w-full grow">
         <h1 class="w-full mb-5 text-4xl">テキストから</h1>
         <textarea
+            bind:value={$textareaValue}
             class="rounded-lg border-2 border-sky-500 w-full h-80 resize-none"
             placeholder="予定の情報を入力"
         />
     </section>
     <section class="w-full">
-        <SubmitButton>送信</SubmitButton>
+        <SubmitButton on:click={handleClick}>送信</SubmitButton>
     </section>
 </main>
