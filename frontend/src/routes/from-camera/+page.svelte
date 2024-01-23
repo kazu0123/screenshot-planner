f33f1c0eed7e120ff641966e33d08c9b0688ff7a
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-01-23 16:29:18 +0000

    カメラから画像を読み込む機能を追加

--- frontend/src/routes/from-camera/+page.svelte
+++ frontend/src/routes/from-camera/+page.svelte
@@ -4,13 +4,31 @@
     import { writable } from "svelte/store";
 
     const processing = writable(false);
+
+    let files: FileList;
+    let imageURI = "";
+
+    function updateImageURI(event: Event) {
+        URL.revokeObjectURL(imageURI);
+
+        const file = files.item(0);
+        if (file == null) { return }
+
+        imageURI = URL.createObjectURL(file);
+    }
 </script>
 
 <main class="flex flex-col w-96 h-svh p-5">
     <section class="w-full grow">
         <h1 class="w-full mb-5 text-4xl">画像から</h1>
+        {#if imageURI == ""}
+            <p>画像をアップロードしてください</p>
+        {:else}
+            <img src="{imageURI}" alt="" class="w-full">
+        {/if}
     </section>
     <section class="w-full">
+        <input bind:files={files} type="file" capture="environment" on:change={updateImageURI}>
         <SubmitButton disabled={$processing}>
             {#if $processing}
                 <Spinner></Spinner>
