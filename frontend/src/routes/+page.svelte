569c86b4a7c03121ca326be5ba0d48412211d53e
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-01-24 19:00:27 +0000

    fix: delete +pages.svelte main tag

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,11 +2,9 @@
     import BottomButton from "$lib/BottomButton.svelte";
 </script>
 
-<main class="flex justify-center items-center w-96 h-full">
-    <h1 class="relative bottom-24 text-4xl">予定登録アプリ</h1>
-    <ul class="flex absolute bottom-5">
-        <li class="first:ml-0 ml-2"><a href="/from-text"><BottomButton icon="text"></BottomButton></a></li>
-        <li class="first:ml-0 ml-2"><a href="/from-camera"><BottomButton icon="camera"></BottomButton></a></li>
-        <li class="first:ml-0 ml-2"><a href="/from-microphone"><BottomButton icon="microphone"></BottomButton></a></li>
-    </ul>
-</main>
+<h1 class="relative bottom-24 text-4xl">予定登録アプリ</h1>
+<ul class="flex absolute bottom-5">
+    <li class="first:ml-0 ml-2"><a href="/from-text"><BottomButton icon="text"></BottomButton></a></li>
+    <li class="first:ml-0 ml-2"><a href="/from-camera"><BottomButton icon="camera"></BottomButton></a></li>
+    <li class="first:ml-0 ml-2"><a href="/from-microphone"><BottomButton icon="microphone"></BottomButton></a></li>
+</ul>
